0d449d92d9c1513cc09e172e61771469d04b275c
unilock <unilock@fennet.rentals>
2024-07-06 08:42:58 -0400

    init
    Signed-off-by: unilock <unilock@fennet.rentals>

---
+++ src/main/java/cc/unilock/chromatinei/ChromatiNEI.java
@@ -0,0 +1,24 @@
+package cc.unilock.chromatinei;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(
+    modid = ChromatiNEI.MOD_ID,
+    version = Tags.VERSION,
+    name = "ChromatiNEI",
+    acceptedMinecraftVersions = "[1.7.10]",
+    dependencies = "required-after:ChromatiCraft;" +
+        "required-after:NotEnoughItems"
+)
+public class ChromatiNEI {
+    public static final String MOD_ID = "chromatinei";
+    public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        LOGGER.info("Hello from ChromatiNEI!");
+    }
+}
